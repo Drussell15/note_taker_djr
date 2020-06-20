86915aea5d5d2357526d9c604d8f6d48ef8a3d9c
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-06-20 12:20:50 -0700

    Update server.js
    express app to handle data parsing and static

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ const fs = require("fs");
 //set up express app
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//set up express app to handle data parsing and static
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(expess.json());
+
+//make routes
