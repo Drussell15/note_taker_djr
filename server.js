ba32466c8ffb315c4327c3244bca9f74af768278
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-06-20 12:17:52 -0700

    Update server.js
    add dependencies and set up express

---
+++ server.js
@@ -0,0 +1,9 @@
+//dependencies
+
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+//set up express app
+const app = express();
+const PORT = process.env.PORT || 3000;
