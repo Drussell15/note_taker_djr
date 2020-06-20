65bdff602026a7f6f105c80c03d7721b3691e923
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-06-20 12:27:53 -0700

    Update server.js
    added homepage default

--- server.js
+++ server.js
@@ -14,3 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(expess.json());
 
 //make routes
+//change to notes page when start button clicked
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', "index.html"));
+});
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+//default is homepage- this needs to be last in order
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', "index.html"));
+});
